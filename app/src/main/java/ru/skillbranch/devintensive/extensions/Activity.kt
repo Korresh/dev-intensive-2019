b03283f3680e4b3b99c1d91af100443362921df1
korresh <kalegin.andrew@gmail.com>
2019-07-14 12:37:56 +0500

    hometask_3_only_mandatory

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.view.inputmethod.InputMethodManager
 import android.app.Activity as Activity
 
-fun Activity.hideKeyboard() {
+//fun Activity.hideKeyboard() {
 //    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
 //    imm.hideSoftInputFromWindow(findViewById(android.R.id.content).getWindowToken(), 0);
-}
+//}
