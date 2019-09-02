7713c87f5d8696e04064e88d4103bff27d08c6c6
korresh <kalegin.andrew@gmail.com>
2019-09-02 12:36:34 +0500

    hometask5_empty

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -1,12 +1,13 @@
 package ru.skillbranch.devintensive.models
 
-import android.service.voice.AlwaysOnHotwordDetector
+import ru.skillbranch.devintensive.models.data.Chat
+import ru.skillbranch.devintensive.models.data.User
 import java.util.*
 
 abstract class BaseMessage (
     val id:String,
-    val from:User?,
-    val chat:Chat,
+    val from: User?,
+    val chat: Chat,
     val isIncoming: Boolean = false,
     val date:Date = Date()
 
@@ -14,7 +15,7 @@ abstract class BaseMessage (
     abstract fun formatMessage():String
     companion object abstractFactory{
         var lastId = -1
-        fun makeMessage(from: User,chat: Chat,date: Date =Date(),type:String, payload:Any?,isIncoming: Boolean =false): BaseMessage{
+        fun makeMessage(from: User, chat: Chat, date: Date =Date(), type:String, payload:Any?, isIncoming: Boolean =false): BaseMessage{
             lastId++
             return when(type){
                 "image" -> ImageMessage("$lastId",from,chat,date=date,image = payload as String,isIncoming = isIncoming)
--- app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
@@ -1,19 +1,21 @@
 package ru.skillbranch.devintensive.models
 
 import ru.skillbranch.devintensive.extensions.humanizeDiff
+import ru.skillbranch.devintensive.models.data.Chat
+import ru.skillbranch.devintensive.models.data.User
 import java.util.*
 
 class TextMessage(
     id:String,
-    from:User,
-    chat:Chat,
+    from: User,
+    chat: Chat,
     isIncoming : Boolean = false,
     date:Date = Date(),
+    isReaded:Boolean = false,
     var text:String?
 ):BaseMessage(id,from,chat,isIncoming,date)
 {
-    override fun formatMessage(): String ="id:$id ${from?.firstName}" +
-
-       " ${if(isIncoming)"получил" else "отправил"} сообщение \"$text\"${date.humanizeDiff()}"
+  override fun formatMessage(): String ="id:$id ${from?.firstName}" +
+          " ${if(isIncoming)"получил" else "отправил"} сообщение \"$text\"${date.humanizeDiff()}"
 
 }
